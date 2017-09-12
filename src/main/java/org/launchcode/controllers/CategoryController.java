e36f68d4f6e25b414992682c10582363590cc90c
ctam91 <ctam91@gmail.com>
2017-09-12 15:47:45 -0700

    add new category form

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,11 +1,17 @@
 package org.launchcode.controllers;
 
 
+import org.launchcode.models.Category;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("category")
@@ -21,5 +27,27 @@ public class CategoryController {
         model.addAttribute("categories", categoryDao.findAll());
         model.addAttribute("title", "Categories" );
         return "category/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddForm(Model model){
+
+        model.addAttribute(new Category());
+        model.addAttribute("title", "Add Category");
+        return "category/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheese(@ModelAttribute @Valid Category category, Errors errors, Model model){
+
+        if(errors.hasErrors()){
+            model.addAttribute(new Category());
+            model.addAttribute("title", "Add Category");
+            return "category/add";
+        }
+        categoryDao.save(category);
+
+        // Redirect to /category
+        return "redirect:";
     }
 }
