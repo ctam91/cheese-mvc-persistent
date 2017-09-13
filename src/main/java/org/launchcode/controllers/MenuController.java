5edac951be9ddd40fde584bcf4848080c0b3bffe
ctam91 <ctam91@gmail.com>
2017-09-13 14:16:41 -0700

    set up menu pathway

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -2,17 +2,28 @@ package org.launchcode.controllers;
 
 import org.launchcode.models.data.CategoryDao;
 import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "menu")
+@RequestMapping("menu")
 public class MenuController {
 
     @Autowired
     private CheeseDao cheeseDao;
 
     @Autowired
-    private CategoryDao categoryDao;
+    private MenuDao menuDao;
+
+    // Request path: /menu
+    @RequestMapping(value = "")
+    public String index(Model model){
+
+        model.addAttribute("Menus", menuDao.findAll());
+        model.addAttribute("title", "Menus" );
+        return "menu/index";
+    }
 }
